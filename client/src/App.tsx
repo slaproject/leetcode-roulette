625a0766a810fde41c24756eb38da579f392adb4
Ishan Vilas Sarode <57068553+Ishan1414@users.noreply.github.com>
2025-11-09 16:51:13 -0500

    Remove React import from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Button } from './components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
 
